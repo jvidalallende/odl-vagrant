d315877cf51f89b1d12b1947a7712f947c5c7def
Juan Vidal <juanvidalallende@gmail.com>
2017-03-03 09:16:47 +0100

    Clone & compile patched OvS 2.6.1 with NSH support
    Signed-off-by: Juan Vidal <juanvidalallende@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
+# Setup your username for OpenDaylight's gerrit
+ODL_USERNAME=JuanVidal
+
 echo "Installing dependencies..."
+sudo apt-get update -y
 sudo apt-get install -y git openjdk-8-jdk-headless maven tmux git-review
 
 echo "Configuring environment variables..."
 echo "export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64" >> ~/.bash_profile
-echo "export MAVEN_OPTS='-Xmx1048m'" >> ~/.bash_profile
+echo "export MAVEN_OPTS='-Xmx1024m'" >> ~/.bash_profile
 
 echo "Retrieving Opendaylight's Maven settings.xml..."
 mkdir -p ~/.m2/
@@ -25,3 +29,26 @@ echo '. ~/.bashrc' >> ~/.bash_profile
 echo "Cloning SFC and NetVirt repositories..."
 git clone https://github.com/opendaylight/sfc.git ~/odl-sfc
 git clone https://github.com/opendaylight/netvirt.git ~/odl-netvirt
+git clone https://github.com/opendaylight/docs.git ~/odl-docs
+
+echo "Installing git hooks..."
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -p -P 29418 $ODL_USERNAME@git.opendaylight.org:hooks/commit-msg /tmp/commit-msg
+chmod 755 /tmp/commit-msg
+cp /tmp/commit-msg ~/odl-sfc/.git/hooks/commit-msg
+cp /tmp/commit-msg ~/odl-netvirt/.git/hooks/commit-msg
+
+echo "Building OvS with NSH support..."
+sudo apt-get install -y libtool m4 autoconf automake make libssl-dev libcap-ng-dev python3 python-six vlan iptables
+git clone https://github.com/openvswitch/ovs.git
+git clone https://github.com/yyang13/ovs_nsh_patches.git
+cd ovs
+git reset --hard v2.6.1
+cp ../ovs_nsh_patches/v2.6.1/*.patch ./
+git config user.email "nsh_patcher@ovs_user.com"
+git config user.name "nsh_patcher"
+git am *.patch
+./boot.sh
+./configure --with-linux=/lib/modules/`uname -r`/build
+sudo make modules_install
+sudo make install
+cd ~
